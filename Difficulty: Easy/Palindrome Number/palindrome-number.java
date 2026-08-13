b6acad5f50c6a89f1711d2391eb1da812334d457
SakshiSantoshShinde <sakshishinde0923@gmail.com>
2026-08-13 08:30:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Number/palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        int num=n;
+        int rem=0;
+        int rev=0;
+        while(n!=0)
+        {
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        if(rev==num)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
